efdc41189d3b48e665696f14d524ced3dd9dae9e
HisuiKitsune <foxnightly2010@gmail.com>
2022-11-13 17:35:37 -0300

    feat: update and remove functions

--- src/main/java/com/angular/angular14spring/controller/ClientController.java
+++ src/main/java/com/angular/angular14spring/controller/ClientController.java
@@ -6,9 +6,11 @@ import com.angular.angular14spring.model.Client;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -47,5 +49,29 @@ public class ClientController {
         // return ResponseEntity.status(HttpStatus.CREATED)
         //    .body(clientRepository.save(client));
 
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> update(@PathVariable Long id, @RequestBody Client client)
+    {
+        return clientRepository.findById(id)
+            .map(clientFound -> {
+                clientFound.setName(client.getName());
+                clientFound.setCpf(client.getCpf());
+                clientFound.setEmail(client.getEmail());
+                clientFound.setPhone(client.getPhone());
+                Client updated = clientRepository.save(clientFound);
+                return ResponseEntity.ok().body(updated);
+            })
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return clientRepository.findById(id)
+        .map(clientFound -> {
+            clientRepository.deleteById(id);
+            return ResponseEntity.noContent().<Void>build();
+        })
+        .orElse(ResponseEntity.notFound().build());
     }
  }
